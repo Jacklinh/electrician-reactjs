5a497cf0ad87651b190f848225fdab20c920dc6a
Jacklinh <jacklinh79@gmail.com>
2023-09-06 12:15:23 +0700

    add parameter product

--- electrician/src/components/ProductParameter/index.tsx
+++ electrician/src/components/ProductParameter/index.tsx
@@ -1,15 +1,28 @@
 import React from 'react'
 import styles from './ProductParameter.module.css';
-const ProductParameter = () => {
+import { parameterList } from '../../data/parameterList';
+
+type ParameterProp = {
+    label: string,
+    content: string
+}
+const SingleProductParameter = ({props}:{props: ParameterProp}) => {
+    return (
+        <li className={styles.pro}>
+            <span>{props.label}:</span>
+            <span>{props.content}</span>
+        </li>
+    )
+}
+const ProductParameter = () => { 
   return (
     <div id={styles.product_parameter}>
         <section className={styles.tableparameter} id={styles.tableparameter}>
             <h2 className={styles.section_subtitle}>Thông số sản phẩm</h2>
             <ul className={styles.parameter}>
-                <li className={styles.pro}>
-                    <span>Hệ điều hành:</span>
-                    <span> Android 11 </span>
-                </li>
+                {
+                    parameterList.map((item)=> <SingleProductParameter key={`SingleProductParameter_${item.id}`} props={item} />)
+                }
             </ul>
             <div className={styles.viewparameterfull} id={styles.showall_parameter}>
                 Xem chi tiết
---
+++ electrician/src/data/parameterList.ts
@@ -0,0 +1,7 @@
+export const parameterList = [
+    {id: 1, label: 'Hệ điều hành', content: ' Android 11'},
+    {id: 2, label: 'Camera sau', content: ' Chính 12 MP &amp; Phụ 64 MP, 12 MP'},
+    {id: 3, label: 'Camera trước', content: ' 10 MP '},
+    {id: 4, label: 'CPU', content: ' Exynos 2100 8 nhân '},
+    {id: 5, label: 'RAM', content: ' 8 GB'}
+]
